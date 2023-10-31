79ce3465236b4de09408e75af7e694173022bb2f
CODERSTEAMK <ngugimanu@gmail.com>
2023-10-31 13:18:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,247 @@
-import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+import './styles.css';
+import Logos from './SOC-Logo.png'
+import scones from'./scones.jpg'
+import scones1 from'./scones1.jpg'
+import scones2 from'./scones2.jpg'
+import scones3 from'./scones3.jpg'
+import scones4 from'./scones4.jpg'
+import scones5 from'./scones5.jpg'
+import scones6 from'./scones6.jpg'
+import scones7 from'./scones7.jpg'
+import scones8 from'./scones8.jpg'
+import logo1 from'./logo1.svg'
+
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      
+<div className="bady">
+		<div class="Header">
+			<a href="/" class="LogoWrapper"><img src={logo1} style={{
+         
+          width: 1000,
+          height: 400,
+         
+          display: "block",
+		  
+        }} alt="Scone O'Clock logo" /></a>
+
+
+		<div className="labela">
+			<b><marquee><p style={{fontFamily:"Century Gothic, sans-serif",fontSize:30,color:"orangered"}}>!!! MAMA ALICE BAKERS HUT !!!  CALL 0768730914 FOR FREE DELIVERY WITHIN GITHURAI - KAHAWA -  ROYSAMBU!!!</p></marquee></b>	</div>
+	
+			<p classname="Strap" style={{fontFamily:"Century Gothic, sans-serif",fontSize:30,color:"orangered"}}> The Most Resplendent Of Snacks</p>
+		</div>
+
+		
+		
+			<table>
+			<tr>
+				<td style={{marginLeft:100,color:"aqua",}}><p>wyuyuyuyiyiiuuouopopopopopopop</p></td>
+				
+
+				<td>
+					<pre>
+			<p>FLAVOURED BEEF SAMOSA</p>
+			<p> @sh.50</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones1} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+
+
+<td>
+					<pre>
+			<p>SANDWICH VEGGIES</p>
+			<p> @sh.40</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones2} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+</tr>
+
+<div classname="boda"><p style={{color:"aqua",}}>w</p></div>
+
+<tr>
+				<td style={{marginLeft:100,color:"aqua",}}><p>wyuyuyuyiyiiuuouopopopopopopop</p></td>
+				
+
+				<td>
+					<pre>
+			<p>ROASTED CHICKEN /FRIED CHICKEN</p>
+			<p> @sh.100</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones3} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+
+
+<td>
+					<pre>
+			<p>KEBAB ON A STICK</p>
+			<p> @sh.90</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones4} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+</tr>
+
+
+<div classname="boda"><p style={{color:"aqua",}}>w</p></div>
+
+<tr>
+				<td style={{marginLeft:100,color:"aqua",}}><p>wyuyuyuyiyiiuuouopopopopopopop</p></td>
+				
+
+				<td>
+					<pre>
+			<p>MILK-CAKE</p>
+			<p> @sh.75</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones5} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+
+
+<td>
+					<pre>
+			<p>CHOCOLATE-YELLOW-CAKE</p>
+			<p> @sh.75</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones6} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+</tr>
+
+
+
+
+<div classname="boda"><p style={{color:"aqua",}}>w</p></div>
+
+<tr>
+				<td style={{marginLeft:100,color:"aqua",}}><p>wyuyuyuyiyiiuuouopopopopopopop</p></td>
+				
+
+				<td>
+					<pre>
+			<p>LEMON-CUPCAKES</p>
+			<p> @sh.35</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones7} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+
+
+<td>
+					<pre>
+			<p>CHOCOLATE-MILK-SHAKE</p>
+			<p> the scone is a quintessentially British classic.</p>
+			
+			</pre>
+			</td>
+			<td>
+			<img src={scones8} style={{
+          borderRadius: "50%",
+          width: 200,
+          height: 200,
+          background: "red",
+          display: "block",
+		  
+        }} alt="Guardians of the Galaxy"/>
+</td>
+
+</tr>
+				</table>
+
+
+
+<div className="finisher">
+	<p style={{color:"white",display:"flex",alignContent:"center",justifyContent:"center"}}>@developed and produced by:</p>
+	<p style={{color:"white",display:"flex",alignContent:"center",justifyContent:"center"}}>NGUGI MUCHIRI SOFTWARE</p>
+	<p style={{color:"white",display:"flex",alignContent:"center",justifyContent:"center"}}>CALL:0768730914</p>
+	<p style={{color:"white",display:"flex",alignContent:"center",justifyContent:"center"}}>EMAIL:emanungugi@gmail.com</p>
+</div>
+				
+			</div>
+		
+    
+
   );
 }
 
